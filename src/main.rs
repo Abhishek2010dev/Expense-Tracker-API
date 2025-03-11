63bb9afa8eb659405d36cac9f8f111e76cc34b01
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-11 16:49:47 +0000

    using dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use anyhow::Context;
+use dotenv::dotenv;
 use expense_tracker_api::{config::AppConfig, server};
 use tracing::Level;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    dotenv().context("Failed to load .env")?;
     tracing_subscriber::fmt()
         .with_max_level(Level::DEBUG)
         .init();
