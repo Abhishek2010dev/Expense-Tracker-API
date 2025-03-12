b35e8f1379b1e85a29f8aef28d9d5040c1d6c99e
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-12 11:02:30 +0000

    EnvProvider trait

--- src/config/env_config.rs
+++ src/config/env_config.rs
@@ -1,54 +1,17 @@
 use std::{borrow::Cow, env};
 
-use super::{Config, error::ConfigError};
+use super::error::ConfigError;
 
-#[derive(Debug, Clone)]
-pub struct EnvConfig {
-    database_url: Cow<'static, str>,
-    access_secret: Cow<'static, str>,
-    refresh_secret: Cow<'static, str>,
-    redis_url: Cow<'static, str>,
-    port: u16,
+pub trait EnvProvider {
+    fn get(&self, key: &'static str) -> Result<Cow<'static, str>, ConfigError>;
 }
 
-impl EnvConfig {
-    pub fn new() -> Result<Self, ConfigError> {
-        Ok(Self {
-            database_url: get_env("DATABASE_URL")?,
-            access_secret: get_env("ACCESS_SECRET")?,
-            refresh_secret: get_env("REFRESH_SECRET")?,
-            redis_url: get_env("REDIS_URL")?,
-            port: get_env("PORT")?
-                .parse::<u16>()
-                .map_err(ConfigError::InvalidPort)?,
-        })
-    }
-}
+pub struct StdEnv;
 
-impl Config for EnvConfig {
-    fn database_url(&self) -> &str {
-        &self.database_url
+impl EnvProvider for StdEnv {
+    fn get(&self, key: &'static str) -> Result<Cow<'static, str>, ConfigError> {
+        env::var(key)
+            .map(Cow::Owned)
+            .map_err(|_| ConfigError::MissingVar(key))
     }
-
-    fn access_secret(&self) -> &str {
-        &self.access_secret
-    }
-
-    fn refresh_secret(&self) -> &str {
-        &self.refresh_secret
-    }
-
-    fn redis_url(&self) -> &str {
-        &self.redis_url
-    }
-
-    fn port(&self) -> u16 {
-        self.port
-    }
-}
-
-fn get_env(var: &'static str) -> Result<Cow<'static, str>, ConfigError> {
-    env::var(var)
-        .map(Cow::Owned)
-        .map_err(|_| ConfigError::MissingVar(var))
 }
--- src/config/mod.rs
+++ src/config/mod.rs
@@ -8,3 +8,55 @@ pub trait Config {
     fn redis_url(&self) -> &str;
     fn port(&self) -> u16;
 }
+
+use env_config::EnvProvider;
+use error::ConfigError;
+use std::{borrow::Cow, env};
+
+#[derive(Debug, Clone)]
+pub struct EnvConfig<P: EnvProvider> {
+    provider: P,
+    database_url: Cow<'static, str>,
+    access_secret: Cow<'static, str>,
+    refresh_secret: Cow<'static, str>,
+    redis_url: Cow<'static, str>,
+    port: u16,
+}
+
+impl<P: EnvProvider> EnvConfig<P> {
+    pub fn new(provider: P) -> Result<Self, ConfigError> {
+        Ok(Self {
+            database_url: provider.get("DATABASE_URL")?,
+            access_secret: provider.get("ACCESS_SECRET")?,
+            refresh_secret: provider.get("REFRESH_SECRET")?,
+            redis_url: provider.get("REDIS_URL")?,
+            port: provider
+                .get("PORT")?
+                .parse::<u16>()
+                .map_err(ConfigError::InvalidPort)?,
+            provider,
+        })
+    }
+}
+
+impl<P: EnvProvider> Config for EnvConfig<P> {
+    fn database_url(&self) -> &str {
+        &self.database_url
+    }
+
+    fn access_secret(&self) -> &str {
+        &self.access_secret
+    }
+
+    fn refresh_secret(&self) -> &str {
+        &self.refresh_secret
+    }
+
+    fn redis_url(&self) -> &str {
+        &self.redis_url
+    }
+
+    fn port(&self) -> u16 {
+        self.port
+    }
+}
