01413fffdb5131504912dcd52f166daa762596d2
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-13 12:57:06 +0530

    PgDatabase strcut

--- src/database.rs
+++ src/database.rs
@@ -1,10 +1,33 @@
+use std::{sync::Arc, time::Duration};
+
 use anyhow::Result;
-use sqlx::{Database, Pool};
+use sqlx::{Database, Pool, Postgres, postgres::PgPoolOptions};
 
 pub trait DatabaseConnection<DB: Database> {
     async fn connect(database_url: &str) -> Result<Self>
     where
         Self: Sized;
 
-    fn pool(&self) -> Pool<DB>;
+    fn pool(&self) -> &Pool<DB>;
+}
+
+pub struct PgDatabase(Arc<Pool<Postgres>>);
+
+impl DatabaseConnection<Postgres> for PgDatabase {
+    async fn connect(database_url: &str) -> Result<Self>
+    where
+        Self: Sized,
+    {
+        let pool = PgPoolOptions::new()
+            .max_connections(5)
+            .acquire_timeout(Duration::from_secs(3))
+            .connect(database_url)
+            .await
+            .expect("can't connect to database");
+        Ok(Self(Arc::new(pool)))
+    }
+
+    fn pool(&self) -> &Pool<Postgres> {
+        &self.0
+    }
 }
