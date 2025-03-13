8e1c2aaaddc744990cc0fbbd173097f975b0a787
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-13 22:40:27 +0530

    using pg and redis object in Server struct

--- src/server.rs
+++ src/server.rs
@@ -1,16 +1,33 @@
 use anyhow::{Context, Ok};
 use axum::Router;
+use sqlx::Postgres;
 use tokio::net::TcpListener;
 
-use crate::config::Config;
+use crate::{
+    config::Config,
+    database::{DatabaseConnection, PgDatabase},
+    redis::{CacheConnection, RedisClient},
+};
 
-pub struct Server<C: Config> {
+pub struct Server<C, D, R>
+where
+    C: Config,
+    D: DatabaseConnection<Postgres>,
+    R: CacheConnection,
+{
     config: C,
+    db: D,
+    redis: R,
 }
-
-impl<C: Config> Server<C> {
-    pub fn new(config: C) -> Self {
-        return Self { config };
+impl<C: Config> Server<C, PgDatabase, RedisClient> {
+    pub async fn new(config: C) -> anyhow::Result<Self> {
+        let db = PgDatabase::connect(config.database_url())
+            .await
+            .context("Failed to create PgDatabase")?;
+        let redis = RedisClient::connect(config.redis_url())
+            .await
+            .context("Failed to create RedisClient")?;
+        return Ok(Self { config, db, redis });
     }
 
     pub async fn run(&self) -> anyhow::Result<()> {
