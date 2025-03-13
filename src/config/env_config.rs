5ca3d6c1f1e92502476f16e6d9449450cffb7fdc
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-13 12:16:53 +0530

    moving config trait

--- src/config/env_config.rs
+++ src/config/env_config.rs
@@ -1,5 +1,14 @@
-use super::{Config, env_provider::EnvProvider, error::ConfigError};
+use super::{env_provider::EnvProvider, error::ConfigError};
 use std::borrow::Cow;
+
+pub trait Config {
+    fn database_url(&self) -> &str;
+    fn access_secret(&self) -> &str;
+    fn refresh_secret(&self) -> &str;
+    fn redis_url(&self) -> &str;
+    fn host(&self) -> &str;
+    fn port(&self) -> u16;
+}
 
 #[derive(Debug, Clone)]
 pub struct EnvConfig {
