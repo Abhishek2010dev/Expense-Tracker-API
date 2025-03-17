61eb1cb7a28ce40946b21d1b74c78228d5c578d9
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-17 16:42:43 +0530

    Password hash function

--- src/auth/password.rs
+++ src/auth/password.rs
@@ -1,4 +1,29 @@
+use anyhow::{Context, Result, anyhow};
+use argon2::{
+    Argon2,
+    password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, SaltString, rand_core::OsRng},
+};
+
 pub trait PasswordService {
-    fn hash_password(&self, password: &str) -> String;
+    fn hash_password(&self, password: &str) -> anyhow::Result<String>;
     fn verify_password(&self, password: &str, hash: &str) -> bool;
+}
+
+#[derive(Default)]
+pub struct PasswordServiceImpl<'a> {
+    argon2: Argon2<'a>,
+}
+
+impl<'a> PasswordService for PasswordServiceImpl<'a> {
+    fn hash_password(&self, password: &str) -> Result<String> {
+        self.argon2
+            .hash_password(password.as_bytes(), &SaltString::generate(&mut OsRng))
+            .map(|value| value.to_string())
+            .map_err(|err| anyhow!(err))
+            .context("Failed to hash password")
+    }
+
+    fn verify_password(&self, password: &str, hash: &str) -> bool {
+        todo!()
+    }
 }
