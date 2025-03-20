912294c19539ffc0db4cbf507b044941ce4b749f
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-20 19:40:16 +0530

    error fix

--- src/user/repository.rs
+++ src/user/repository.rs
@@ -32,21 +32,21 @@ impl UserRespository for UserRespositoryImpl {
             payload.email,
             payload.password_hash
         )
-        .fetch_one(&self.pool)
+        .fetch_one(&*self.pool)
         .await
         .context("Failed to create user")
     }
 
     async fn find_by_id(&self, id: i32) -> Result<Option<User>> {
         sqlx::query_as!(User, "SELECT * FROM users WHERE id = $1", id)
-            .fetch_optional(&self.pool)
+            .fetch_optional(&*self.pool)
             .await
             .context(format!("Failed to find user by id: {}", id))
     }
 
     async fn exits(&self, id: i32) -> Result<bool> {
         let exists = sqlx::query_scalar!("SELECT EXISTS(SELECT 1 FROM users WHERE id = $1)", id)
-            .fetch_one(&self.pool)
+            .fetch_one(&*self.pool)
             .await?;
         Ok(exists.unwrap_or(false))
     }
