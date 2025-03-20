9aacee5de75a87399eec98b06c28513a81edf56d
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-20 14:58:11 +0530

    RefreshTokenRepository token

--- src/auth/token/repository/refresh_token.rs
+++ src/auth/token/repository/refresh_token.rs
@@ -1,6 +1,14 @@
+use anyhow::Result;
+use async_trait::async_trait;
+use fred::prelude::Client;
 use std::sync::Arc;
 
-use fred::prelude::Client;
+#[async_trait]
+pub trait RefreshTokenRepository {
+    async fn store_refresh_token(&self, user_id: i32, token: &str, ttl: u64) -> Result<()>;
+    async fn get_refresh_token(&self, user_id: i32) -> Result<Option<String>>;
+    async fn delete_refresh_token(&self, user_id: i32) -> Result<()>;
+}
 
 pub struct RedisRefreshTokenRepository {
     client: Arc<Client>,
