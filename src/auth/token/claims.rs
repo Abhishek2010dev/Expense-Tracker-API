6b80cfd55b50d0d42669dc4429b46fdeb512546f
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-20 19:18:23 +0530

    new function for cliams

--- src/auth/token/claims.rs
+++ src/auth/token/claims.rs
@@ -1,7 +1,20 @@
+use anyhow::Ok;
+use chrono::Duration;
 use serde::{Deserialize, Serialize};
+
+use super::utils::generate_expiration;
 
 #[derive(Serialize, Deserialize)]
 pub struct Claims {
     pub sub: i32,
     pub exp: usize,
+}
+
+impl Claims {
+    pub fn new(sub: i32, duration: Duration) -> anyhow::Result<Self> {
+        Ok(Self {
+            sub,
+            exp: generate_expiration(duration)?,
+        })
+    }
 }
