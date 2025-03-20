81684914d9a40ab7b49f8e3a1b4a8d1c52c76483
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-20 14:56:25 +0530

    RedisRefreshTokenRepository struct

--- src/auth/token/repository/refresh_token.rs
+++ src/auth/token/repository/refresh_token.rs
@@ -1,1 +1,13 @@
-pub struct RefreshTokenRepository {}
+use std::sync::Arc;
+
+use fred::prelude::Client;
+
+pub struct RedisRefreshTokenRepository {
+    client: Arc<Client>,
+}
+
+impl RedisRefreshTokenRepository {
+    pub fn new(client: Arc<Client>) -> RedisRefreshTokenRepository {
+        return RedisRefreshTokenRepository { client };
+    }
+}
