116efabe50e3b37707f88ccbff644b0cc3465c48
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-20 17:42:19 +0530

    refresh token service with secret_key

--- src/auth/token/service/refresh_token.rs
+++ src/auth/token/service/refresh_token.rs
@@ -1,11 +1,17 @@
+use std::sync::Arc;
+
 use crate::auth::token::repository::refresh_token::RefreshTokenRepository;
 
 pub struct RefreshTokenService<R: RefreshTokenRepository> {
     repository: R,
+    secret_key: Arc<Vec<u8>>,
 }
 
 impl<R: RefreshTokenRepository> RefreshTokenService<R> {
-    pub fn new(repository: R) -> Self {
-        return Self { repository };
+    pub fn new(repository: R, secret_key: impl Into<Vec<u8>>) -> Self {
+        return Self {
+            repository,
+            secret_key: Arc::new(secret_key.into()),
+        };
     }
 }
