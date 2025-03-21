27aa0ae5a6450f57e15d31659ae86170a6be7527
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-21 16:48:15 +0530

    refresh response struct

--- src/auth/handler/register.rs
+++ src/auth/handler/register.rs
@@ -1,5 +1,20 @@
+use std::sync::Arc;
+
+use axum::{Json, extract::State, http::StatusCode};
+use axum_valid::Valid;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use validator::Validate;
+
+use crate::{
+    auth::{
+        password::PasswordService,
+        token::service::{access_token::AccessTokenService, refresh_token::RefreshTokenService},
+    },
+    error::AppError,
+    state::AppState,
+    user::{repository::UserRepository, utils::CreateUserPayload},
+};
 
 #[derive(Debug, Validate, Deserialize, Serialize)]
 pub struct RegisterPayload {
@@ -11,4 +26,37 @@ pub struct RegisterPayload {
 
     #[validate(length(min = 8, message = "Password must be at least 8 characters long"))]
     password: String,
+}
+
+pub async fn register(
+    State(state): State<Arc<AppState>>,
+    Valid(Json(payload)): Valid<Json<RegisterPayload>>,
+) -> Result<(StatusCode, Json<Value>), AppError> {
+    if state
+        .user_repository
+        .exists_by_email(&payload.email)
+        .await?
+    {
+        return Err(AppError::Conflict("User already exits".into()));
+    }
+
+    let password_hash = state.password_service.hash_password(&payload.password)?;
+    let user = state
+        .user_repository
+        .create(CreateUserPayload {
+            name: payload.name,
+            email: payload.email,
+            password_hash,
+        })
+        .await?;
+
+    let access_token = state.access_token_service.generate_token(user.id).await?;
+    let refresh_token = state.refresh_token_service.generate_token(user.id).await?;
+
+    let response = serde_json::json!({
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+    });
+
+    Ok((StatusCode::CREATED, Json(response)))
 }
--- src/error.rs
+++ src/error.rs
@@ -7,19 +7,19 @@ use serde_json::json;
 use thiserror::Error;
 use tracing::error;
 
-#[derive(Debug, Error)]
+#[derive(Debug, thiserror::Error)]
 pub enum AppError {
-    #[error("Database error: {0}")]
-    DatabaseError(#[from] sqlx::Error),
-
     #[error("Not Found: {0}")]
     NotFound(String),
 
     #[error("Bad Request: {0}")]
     BadRequest(String),
 
-    #[error("Internal Server Error")]
-    InternalServerError,
+    #[error("{0}")]
+    Conflict(String),
+
+    #[error("Internal Server Error: {0}")]
+    InternalServerError(#[from] anyhow::Error),
 }
 
 impl IntoResponse for AppError {
@@ -27,16 +27,13 @@ impl IntoResponse for AppError {
         error!("{}", self);
 
         let (status, error_message) = match &self {
-            AppError::DatabaseError(_) => (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                "A database error occurred.",
-            ),
             AppError::NotFound(message) => (StatusCode::NOT_FOUND, message.as_str()),
             AppError::BadRequest(message) => (StatusCode::BAD_REQUEST, message.as_str()),
-            AppError::InternalServerError => (
+            AppError::InternalServerError(_) => (
                 StatusCode::INTERNAL_SERVER_ERROR,
                 "An internal server error occurred.",
             ),
+            AppError::Conflict(msg) => (StatusCode::CONFLICT, msg.as_str()),
         };
 
         let body = Json(json!({ "error": error_message }));
