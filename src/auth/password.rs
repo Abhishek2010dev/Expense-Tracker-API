457fda26b16aed29409dd6e4005b38465a821384
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-21 09:45:11 +0530

    bug fix

--- src/auth/password.rs
+++ src/auth/password.rs
@@ -1,36 +1,43 @@
-use std::sync::Arc;
-
-use anyhow::{Context, Result, anyhow};
+use anyhow::{Context, Result};
 use argon2::{
     Argon2,
     password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, SaltString, rand_core::OsRng},
 };
 
 pub trait PasswordService {
-    fn hash_password(&self, password: &str) -> anyhow::Result<String>;
+    fn hash_password(&self, password: &str) -> Result<String>;
     fn verify_password(&self, password: &str, hash: &str) -> bool;
 }
 
 #[derive(Default)]
-pub struct PasswordServiceImpl<'a> {
-    argon2: Arc<Argon2<'a>>,
+pub struct PasswordServiceImpl {
+    argon2: Argon2<'static>,
 }
 
-impl<'a> PasswordService for PasswordServiceImpl<'a> {
+impl PasswordServiceImpl {
+    pub fn new() -> Self {
+        Self {
+            argon2: Argon2::default(),
+        }
+    }
+}
+
+impl PasswordService for PasswordServiceImpl {
     fn hash_password(&self, password: &str) -> Result<String> {
+        let salt = SaltString::generate(&mut OsRng);
         self.argon2
-            .hash_password(password.as_bytes(), &SaltString::generate(&mut OsRng))
-            .map(|value| value.to_string())
-            .map_err(|err| anyhow!(err))
+            .hash_password(password.as_bytes(), &salt)
+            .map(|hashed| hashed.to_string())
             .context("Failed to hash password")
     }
 
     fn verify_password(&self, password: &str, hash: &str) -> bool {
-        PasswordHash::new(hash)
-            .and_then(|parsed_hash| {
-                self.argon2
-                    .verify_password(password.as_bytes(), &parsed_hash)
-            })
-            .is_ok()
+        match PasswordHash::new(hash) {
+            Ok(parsed_hash) => self
+                .argon2
+                .verify_password(password.as_bytes(), &parsed_hash)
+                .is_ok(),
+            Err(_) => false,
+        }
     }
 }
