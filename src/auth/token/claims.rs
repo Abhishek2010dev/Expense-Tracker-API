9b3a68b5cec35641103ae1556bdfca917b53ce30
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-21 21:09:15 +0530

    claims a extract

--- src/auth/token/claims.rs
+++ src/auth/token/claims.rs
@@ -1,6 +1,19 @@
-use anyhow::Ok;
+use async_trait::async_trait;
+use axum::{
+    RequestPartsExt,
+    extract::{FromRef, FromRequestParts},
+    http::request::Parts,
+};
+use axum_extra::{
+    TypedHeader,
+    headers::{Authorization, authorization::Bearer},
+};
 use chrono::Duration;
 use serde::{Deserialize, Serialize};
+
+use crate::{
+    auth::token::service::access_token::AccessTokenService, error::AppError, state::AppState,
+};
 
 use super::utils::generate_expiration;
 
@@ -16,5 +29,45 @@ impl Claims {
             sub,
             exp: generate_expiration(duration)?,
         })
+    }
+}
+
+impl<S> FromRequestParts<S> for Claims
+where
+    AppState: FromRef<S>,
+    S: Send + Sync,
+{
+    type Rejection = AppError;
+
+    async fn from_request_parts(parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
+        let state = AppState::from_ref(state);
+
+        let TypedHeader(Authorization(bearer)) = parts
+            .extract::<TypedHeader<Authorization<Bearer>>>()
+            .await
+            .map_err(|_| AppError::BadRequest("Invalid token".into()))?;
+
+        match state
+            .access_token_service
+            .validate_token(bearer.token())
+            .await
+        {
+            Ok(claims) => Ok(claims),
+            Err(err) => match err {
+                crate::auth::token::error::TokenValidationError::Expired => {
+                    Err(AppError::Unauthorized("Token has expired".into()))
+                }
+                crate::auth::token::error::TokenValidationError::InvalidFormat => {
+                    Err(AppError::BadRequest("Token format is invalid".into()))
+                }
+                crate::auth::token::error::TokenValidationError::InvalidSignature => {
+                    Err(AppError::Unauthorized("Token signature is invalid".into()))
+                }
+                crate::auth::token::error::TokenValidationError::ValidationFailed => {
+                    Err(AppError::Unauthorized("Token validation failed".into()))
+                }
+                crate::auth::token::error::TokenValidationError::RedisTokenNull => unreachable!(),
+            },
+        }
     }
 }
