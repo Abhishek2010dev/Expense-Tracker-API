5cc608e6e21e88aa80ca98c529524eda3d300979
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-22 21:51:10 +0530

    expense rust models

--- src/expense/models.rs
+++ src/expense/models.rs
@@ -1,1 +1,25 @@
+use chrono::NaiveDateTime;
+use serde::{Deserialize, Serialize};
+use sqlx::Type;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize, Type)]
+#[sqlx(type_name = "expense_category", rename_all = "snake_case")]
+pub enum ExpenseCategory {
+    Groceries,
+    Leisure,
+    Electronics,
+    Utilities,
+    Clothing,
+    Health,
+    Others,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Expense {
+    id: i32,
+    user_id: i32,
+    category: ExpenseCategory,
+    amount: f64,
+    description: Option<String>,
+    expense_date: NaiveDateTime,
+}
