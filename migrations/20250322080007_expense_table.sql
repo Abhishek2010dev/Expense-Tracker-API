9ba8ed7b2e9ec1159abdc8197d4c11b506f1e894
Abhishek2010DevSingh <Abhishekkumar2010dev@gmail.com>
2025-03-22 21:42:53 +0530

    expense migration

--- migrations/20250322080007_expense_table.sql
+++ migrations/20250322080007_expense_table.sql
@@ -1,25 +1,19 @@
-CREATE TABLE IF NOT EXISTS expense_categories (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) UNIQUE NOT NULL
+CREATE TYPE expense_category AS ENUM (
+    'Groceries',
+    'Leisure',
+    'Electronics',
+    'Utilities',
+    'Clothing',
+    'Health',
+    'Others'
 );
 
-INSERT INTO expense_categories (name) VALUES 
-    ('Groceries'),
-    ('Leisure'),
-    ('Electronics'),
-    ('Utilities'),
-    ('Clothing'),
-    ('Health'),
-    ('Others')
-ON CONFLICT (name) DO NOTHING;
-
-CREATE TABLE IF NOT EXISTS expenses (
+CREATE TABLE expenses (
     id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
-    category_id INT NOT NULL,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    category expense_category NOT NULL,
     amount DECIMAL(10,2) NOT NULL,
     description TEXT,
-    expense_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (category_id) REFERENCES expense_categories(id) ON DELETE SET NULL
+    expense_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+;
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ mod auth;
 pub mod config;
 mod database;
 mod error;
+mod expense;
 mod redis;
 pub mod server;
 mod state;
